e09e54a1599217033c89e71010efab02eb5898ff
Andy Byers <andy.byers@ubiquitypress.com>
2014-11-05 10:43:29 +0000

    Renamed some stuff."

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ def readme():
         return f.read()
 
 kwargs = {
-	'name': 'sqla-ojs',
+	'name': 'ojs-sqla',
 	'version': '0.0.1',
 	'description': "Comfortable clothing for PKPs OJS",
 	'keywords': 'Ubiquity Press PKP OJS',
 	'author': 'Andy Byers',
 	'author_email': 'andy.byers@ubiquitypress.com',
-	'url': 'https://github.com/ubiquitypress/sqla-ojs',
+	'url': 'https://github.com/ubiquitypress/ojs-sqla',
 	'license': 'GPL 2',
-	'packages': ['sqla-ojs'],
+	'packages': ['ojs-sqla'],
 	
 	# setuptools/pip args
 	
