efa9508672e1cc190852b18cf88760ecfc398c97
Andy Byers <andy.byers@ubiquitypress.com>
2015-04-13 20:54:28 +0100

    Added a new function to create add roles.

--- ojssqla/logic.py
+++ ojssqla/logic.py
@@ -490,4 +490,13 @@ def get_role(session, user_id, role_id):
 def get_ojs_metrics(session, article_id):
 	return session.query(ojs.Metrics).filter(ojs.Metrics.submission_id == article_id).order_by(asc(ojs.Metrics.assoc_type))
 
+def add_role_to_user(session, role, user_id):
+	kwargs = {
+		'journal_id': 1,
+		'user_id': user_id,
+		'role_id': role,
+	}
 
+	new_role = ojs.Roles(**kwargs)
+	session.add(new_role)
+	session.commit() 
