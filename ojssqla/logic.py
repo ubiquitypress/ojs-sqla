b52f6d39fb17dc79e0921e40a049a372f9b6b8c5
mauro <mauro.sanchez@ubiquitypress.com>
2015-04-16 16:54:31 +0100

    Editorial Team: pull 'display_email'

--- ojssqla/logic.py
+++ ojssqla/logic.py
@@ -57,7 +57,9 @@ def editorial_team(session):
 
 	for g in groups:
 		members = session.query(ojs.GroupMemberships).filter(ojs.GroupMemberships.group_id == g.group_id).order_by(ojs.GroupMemberships.seq)
-		group_dict[g.setting_value] = [{'first_name': m.user.first_name, 'last_name': m.user.last_name, 'email': m.user.email, 'url': m.user.url,  'affiliation': get_user_affiliation(session, m.user.user_id), 'bio': get_user_bio(session, m.user.user_id), 'country': m.user.country} for m in members]
+		group = session.query(ojs.Group).filter(ojs.Group.group_id == g.group_id).one()
+		
+		group_dict[g.setting_value] = [{'first_name': m.user.first_name, 'last_name': m.user.last_name, 'email': m.user.email, 'url': m.user.url,  'affiliation': get_user_affiliation(session, m.user.user_id), 'bio': get_user_bio(session, m.user.user_id), 'country': m.user.country, 'display_email': group.publish_email } for m in members]
 
 	return group_dict
 
