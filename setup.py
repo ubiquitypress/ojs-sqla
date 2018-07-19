51e94601bc44757a51cc8456f0c879014a44ee40
James Hurford <james.hurford@ubiquitypress.com>
2018-07-19 12:11:11 +0100

    Bump version: 0.0.5 → 0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 kwargs = {
     'name': 'ojssqla',
-    'version': '0.0.5',
+    'version': '0.1.0',
     'description': "Comfortable clothing for PKPs OJS",
     'keywords': 'Ubiquity Press PKP OJS',
     'author': 'Andy Byers',
